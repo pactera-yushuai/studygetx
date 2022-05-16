1a4dbf5d4ef3c376d3d6b93e4e244063f8cacedb
pactera-sh-yu12 <shuai.yu12@pactera.com>
2022-05-16 08:28:40 +0800

    begin

---
+++ android/app/src/main/kotlin/com/example/studygetx/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.studygetx
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/controllers/controller.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+
+class Controller extends GetxController {
+  var counter = 0.obs;
+
+  void incrementCounter() {
+    counter++;
+  }
+}
